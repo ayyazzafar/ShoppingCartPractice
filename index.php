cff4abe1a60db1665b6c62fd0048b275a7204f56
Ayyaz <ayyaz.zaffar@gmail.com>
2017-06-01 16:54:40 +0500

    Created seperate file for siteInfoBanner to resuse it

--- index.php
+++ index.php
@@ -46,58 +46,7 @@ $base_dir = "";
         </div>
 
 
-        <!-- Slider End-->
-
-        <div id="siteInfo">
-            <div class="container">
-                <div class="row">
-
-
-                    <div class="col-sm-3">
-
-                        <div class="block">
-                            <i class="icon glyphicon glyphicon-send"></i>
-                            <h4>First Shipping</h4>
-                            <h5>Get you everywhere</h5>
-                        </div>
-
-                    </div>
-
-                    <div class="col-sm-3">
-
-                        <div class="block">
-                            <i class="icon glyphicon glyphicon-lock"></i>
-                            <h4>Secure Store</h4>
-                            <h5>100% secure store</h5>
-                        </div>
-
-                    </div>
-
-                    <div class="col-sm-3">
-
-                        <div class="block">
-                            <i class="icon glyphicon glyphicon-time"></i>
-                            <h4>Save Time</h4>
-                            <h5>Save Your Time</h5>
-                        </div>
-
-                    </div>
-
-                    <div class="col-sm-3">
-
-                        <div class="block">
-                            <i class="icon glyphicon glyphicon-shopping-cart"></i>
-                            <h4>Pay on Delivery</h4>
-                            <h5>Get you everywhere</h5>
-                        </div>
-
-                    </div>
-
-
-                </div>
-            </div>
-        </div>
-        <!-- #siteInfo end-->
+        <?php include('includes/siteInfoBanner.php'); ?>
 
         <!-- Tabbed Slider start-->
         <div id="tabbedSlider">
