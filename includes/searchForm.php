0a27b72c2a4b182d99dd7dea7042042e51bc8031
Ayyaz <ayyaz.zaffar@gmail.com>
2017-06-01 16:54:14 +0500

    Header > change position of search form

---
+++ includes/searchForm.php
@@ -0,0 +1,10 @@
+<form class="">
+
+    <div class="form-group pull-left ">
+        <input type="text" placeholder="Search your item..." class="form-control">
+    </div>
+    <button type="submit" class="btn btn-success">
+                                    <i class="glyphicon glyphicon-search"></i>
+
+                                </button>
+</form>
