1b3f269a78f36f97272b67520b02fb8c1a806693
Ayyaz <ayyaz.zaffar@gmail.com>
2017-06-06 13:28:02 +0500

    Producst > Fixed spacing and height issue for product tile

--- pages/products/loop-item.php
+++ pages/products/loop-item.php
@@ -11,40 +11,44 @@
         </div>
         <?php endif; ?>
 
-        <a href="<?php echo $base_dir."pages/products/single.php"; ?>" class="title"><?php echo $product->title; ?></a>
+        <a href="<?php echo $base_dir." pages/products/single.php "; ?>" class="title"><?php echo $product->title; ?></a>
     </div>
-
-    <div class="price <?php echo $product->discount>0?"discounted":'';  ?>   ">
-        $<?php echo number_format($product->price); ?>
-    </div>
-    <?php if(isset($product->discount) && $product->discount > 0):
+    <div class="details">
+        <div class="price <?php echo $product->discount>0?" discounted ":'';  ?>   ">
+            $
+            <?php echo number_format($product->price); ?>
+        </div>
+        <?php if(isset($product->discount) && $product->discount > 0):
     $discount = ($product->discount * $product->price) / 100;
      ?>
         <div class="newPrice">
-            $<?php echo number_format($product->price - $discount); ?>
+            $
+            <?php echo number_format($product->price - $discount); ?>
         </div>
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <div class="rating">
-        <?php for($i=1; $i<=5; $i++): ?>
-        <div class="star <?php 
+        <div class="rating">
+            <?php for($i=1; $i<=5; $i++): ?>
+            <div class="star <?php 
 
         if($i<=$product->rating): 
-            echo "active";
+            echo " active ";
 
         endif;
         
         ?>">
-        </div>
-        <?php endfor; ?>
-        
-    </div>
-    <!-- Rating end-->
+            </div>
+            <?php endfor; ?>
 
-    <a  class="btn  btnStyle1">
+        </div>
+        <!-- Rating end-->
+
+        <a class="btn  btnStyle1">
         <i class="glyphicon glyphicon-shopping-cart
 "></i> Add to Cart
-    </a>
+        </a>
+    </div>
 
-    
+
+
 </div>
